0f52979f68a917af6856e5128feb64a6ccf5678a
brzez <pawel@brzez.com>
2018-01-20 14:40:12 +0100

    changed signature

--- test/Container.test.js
+++ test/Container.test.js
@@ -9,11 +9,11 @@ import Container from '../src/Container'
 
 describe('Container', () => {
   it('should store key->value', () => {
-    const container = new Container();
-
-    container.set('foo', 'bar');
-    container.set('baz', 'bazz');
-
+    const container = new Container({
+      foo: 'bar',
+      baz: 'bazz',
+    });
+    
     expect(container.get('foo')).to.be.equal('bar');
     expect(container.get('baz')).to.be.equal('bazz');
   })
