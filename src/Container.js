dd864373b3762c9c4cf2910e24bc359e4f403da4
brzez <pawel@brzez.com>
2018-01-20 15:12:47 +0100

    container

--- src/Container.js
+++ src/Container.js
@@ -1,3 +1,15 @@
+// @flow
 
+type Registry = {[alias: string]: any};
 
-export default class Container {}
+export default class Container {
+  items: Registry;
+
+  constructor (items: Registry) {
+    this.items = {...items};
+  }
+
+  get (name: string): any {
+    return this.items[name];
+  }
+}
