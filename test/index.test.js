20fb0494f5364363b41756e7d6ac2a13d874e7d5
brzez <pawel@brzez.com>
2018-01-20 22:15:43 +0100

    added new usage for cleanup / refactor

--- test/index.test.js
+++ test/index.test.js
@@ -6,6 +6,25 @@ import assertRejected from './assertRejected'
 
 import boot from '../src/index'
 
+/*
+  usage:
+
+  boot({
+    // will be normalized to Bootable
+    service_1: (Bootable.factory) () => 123, 
+    service_2: (Bootable) {
+      factory,
+      dependencies
+    },
+  })
+  
+  validating service registration:
+    typeof s === function -> is a factory
+    typeof s === object && s.factory === func -> Bootable
+      s.dependencies - optional; set to []
+
+ */
+
 describe('index', () => {
   describe('boot', async () => {
   })
