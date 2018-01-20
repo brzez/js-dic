3762e3dada7d0e1eac921e3d6681d9d525df6630
brzez <pawel@brzez.com>
2018-01-20 14:39:40 +0100

    initial container

---
+++ test/Container.test.js
@@ -0,0 +1,20 @@
+// @flow
+
+import {describe, it} from 'mocha'
+import {expect} from 'chai'
+
+import assertRejected from './assertRejected'
+import Container from '../src/Container'
+
+
+describe('Container', () => {
+  it('should store key->value', () => {
+    const container = new Container();
+
+    container.set('foo', 'bar');
+    container.set('baz', 'bazz');
+
+    expect(container.get('foo')).to.be.equal('bar');
+    expect(container.get('baz')).to.be.equal('bazz');
+  })
+})
