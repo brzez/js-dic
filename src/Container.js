fdec97de7db6bcc1437078d360e5fc99234d7af8
brzez <pawel@brzez.com>
2018-01-20 13:30:54 +0100

    prevent booting more than once

--- src/Container.js
+++ src/Container.js
@@ -10,17 +10,26 @@ type ServiceRegistry = {
 
 export default class Container {
   providers: ServiceRegistry;
+  booted: bool = false;
 
   constructor (providers: ServiceRegistry) {
     this.providers = providers;
   }
 
   async boot () {
+    if (this.booted) {
+      throw new Error('Container already booted');
+    }
+
     const providers: ServiceProvider[] = Object.keys(this.providers)
       .map(alias => this.providers[alias]);
 
     const bootPromises = providers.map(provider => provider());
 
     await Promise.all(bootPromises);
+    
+    this.booted = true;
+
+    return this;
   }
 }
