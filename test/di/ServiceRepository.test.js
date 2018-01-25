61d38e9e9d7fbfe87cdc3209a578b2cc9f69a0b3
brzez <pawel@brzez.com>
2018-01-25 13:54:58 +0100

    cleanup

--- test/di/ServiceRepository.test.js
+++ test/di/ServiceRepository.test.js
@@ -7,73 +7,58 @@ import ServiceRepository from '../../src/di/ServiceRepository'
 import Service from '../../src/di/Service'
 
 const service = (name, tags = [], factory = () => {}, dependencies = []) => new Service(name, tags, factory, dependencies);
+const createRepo = () => {
+  return new ServiceRepository([
+    service('foo', ['a']),
+    service('bar', ['a']),
+    service('baz', ['b']),
+    ])
+}
 
 describe('ServiceRepository', () => {
   describe('findAlias', () => {
 
     it('finds service by name', () => {
-      const sr = new ServiceRepository([
-        service('foo'),
-        service('bar'),
-      ]);
-      const found = sr.findAlias('foo');
+      const sr = createRepo();
+      const result = sr.findAlias('foo');
 
-      expect(found).to.be.an('array');
-      expect(found[0].name).to.equal('foo');
+      expect(result).to.be.an('array');
+      expect(result[0].name).to.equal('foo');
     });
 
     it('throws when service not found (via name)', () => {
-      const sr = new ServiceRepository([]);
-      expect(() => sr.findAlias('foo')).to.throw();
+      const sr = createRepo();
+      expect(() => sr.findAlias('fodzo')).to.throw();
     });
   })
 
   describe('findTags', () => {
     it('finds tags', () => {
-      const sr = new ServiceRepository([
-        service('foo', ['a']),
-        service('bar', ['a']),
-        service('baz', ['b']),
-      ]);
+      const sr = createRepo();
+      const result = sr.findTags('a');
 
-      const found = sr.findTags('a');
-
-      expect(found).to.be.an('array');
-      expect(found).to.have.lengthOf(2);
+      expect(result).to.be.an('array');
+      expect(result).to.have.lengthOf(2);
     });
     it('returns empty array when no tags found', () => {
-      const sr = new ServiceRepository([
-        service('foo', ['a']),
-        service('bar', ['a']),
-        service('baz', ['b']),
-      ]);
-      const found = sr.findTags('z');
+      const sr = createRepo();
+      const result = sr.findTags('z');
 
-      expect(found).to.be.an('array');
-      expect(found).to.have.lengthOf(0);
+      expect(result).to.be.an('array');
+      expect(result).to.have.lengthOf(0);
     });
   })
 
   describe('findDependency', () => {
     it('resolves dependency based on type - service', () => {
-      const sr = new ServiceRepository([
-        service('foo', ['a']),
-        service('bar', ['a']),
-        service('baz', ['b']),
-      ]);
-
+      const sr = createRepo();
       const result = sr.findDependency({name: 'foo', type: 'service'});
 
       expect(result).to.be.an('array');
       expect(result[0].name).to.equal('foo');
     });
     it('resolves dependency based on type - tag', () => {
-      const sr = new ServiceRepository([
-        service('foo', ['a']),
-        service('bar', ['a']),
-        service('baz', ['b']),
-      ]);
-
+      const sr = createRepo();
       const result = sr.findDependency({name: 'a', type: 'tag'});
 
       expect(result).to.be.an('array');
