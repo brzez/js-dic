e08480aa890935d22280dcd82011cb44101520df
brzez <pawel@brzez.com>
2018-02-05 21:06:00 +0100

    normalizeDependencies impl

--- src/normalizeDependencies.js
+++ src/normalizeDependencies.js
@@ -2,5 +2,12 @@
 import type {Dependency} from './di/Dependency'
 
 export default function normalizeDependencies (dependencies: ?Array<Dependency|string>) {
+  dependencies = dependencies || [];
 
+  return dependencies.map(dep => {
+    if (typeof dep === 'string') {
+      return {name: dep, type: 'service'}
+    }
+    return dep;
+  });
 }
