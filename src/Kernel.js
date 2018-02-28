0fe7d9d00e751904746d594193b0d6ff66a15034
brzez <pawel@brzez.com>
2018-02-28 13:11:46 +0100

    kernel wip

--- src/Kernel.js
+++ src/Kernel.js
@@ -3,6 +3,9 @@
 
 import type {ServiceDefinition} from "./types/ServiceDefinition";
 import Container from "./Container";
+import resolveBootOrder from "./resolveBootOrder";
+import mapServices from "./mapServices";
+import {ServiceMap} from "./ServiceMap";
 
 export default class Kernel {
   services: ServiceDefinition[];
@@ -12,6 +15,19 @@ export default class Kernel {
   }
 
   async boot (): Promise<Container> {
+    // resolve boot order
+    const resolved = resolveBootOrder(this.services);
+    const map = mapServices(resolved);
+    // boot all services
+    resolved.forEach(def => {
+      this.bootServiceDefinition(def, map);
+    })
+    // create container w/ booted services
+  }
 
+  bootServiceDefinition(def: ServiceDefinition, map: ServiceMap) {
+    const dependencies = def.dependencies || [];
+    const resolved = dependencies.map(dep => map.resolveDependency(dep));
+    console.log(def, resolved)
   }
 }
--- test/Kernel.test.js
+++ test/Kernel.test.js
@@ -2,7 +2,19 @@
 
 import {describe, it} from 'mocha'
 import {expect} from 'chai'
+import Kernel from "../src/Kernel";
+import mockService from "./mock/mockService";
+import Container from "../src/Container";
 
 describe('Kernel', () => {
-  it('boots all services')
+  it('boots all services', async () => {
+    const k = new Kernel([
+      mockService({name: 'a'}),
+      mockService({name: 'b'}),
+      mockService({name: 'c'}),
+    ]);
+
+    const container = await k.boot();
+    expect(container).to.be.instanceOf(Container);
+  })
 });
